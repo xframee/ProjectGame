a76ee89c5f2d7428c791915d5db1c21b9693e1ee
Alexander <alexander@kmax.dk>
2021-02-17 15:50:55 +0100

    Test pygame

--- game.py
+++ game.py
@@ -1,6 +1,18 @@
 import pygame
+import sys
 
 pygame.init()
 
 displayWidth = 800
 displayHeight = 600
+
+screen = pygame.display.set_mode((displayWidth, displayHeight))
+
+game_over = False
+
+while not game_over:
+
+    for event in pygame.event.get():
+
+        if event.type == pygame.QUIT:
+            sys.exit()
