d6647a858254e5c77fd9b2d04e7de5087c4df235
Alissa Hansen <79077747+AlissaHansen@users.noreply.github.com>
2021-02-20 17:40:19 +0100

    Lavet basic map

--- game.py
+++ game.py
@@ -1,18 +1,84 @@
-import pygame
+import pygame as pg
 import sys
+from settings import *
+from sprites import *
 
-pygame.init()
+class Game:
+    def __init__(self):
+        pg.init()
+        self.screen = pg.display.set_mode((WIDTH, HEIGHT))
+        pg.display.set_caption(TITLE)
+        self.clock = pg.time.Clock()
+        pg.key.set_repeat(500, 100)
+        self.load_data()
 
-displayWidth = 1400
-displayHeight = 800
+    def load_data(self):
+        pass
 
-screen = pygame.display.set_mode((displayWidth, displayHeight))
+    def new(self):
+        # initialize all variables and do all the setup for a new game
+        self.all_sprites = pg.sprite.Group()
+        self.walls = pg.sprite.Group()
+        self.player = Player(self, 10, 10)
+        for x in range(10, 20):
+            Wall(self, x, 5)
 
-game_over = False
+    def run(self):
+        # game loop - set self.playing = False to end the game
+        self.playing = True
+        while self.playing:
+            self.dt = self.clock.tick(FPS) / 1000
+            self.events()
+            self.update()
+            self.draw()
 
-while not game_over:
+    def quit(self):
+        pg.quit()
+        sys.exit()
 
-    for event in pygame.event.get():
+    def update(self):
+        # update portion of the game loop
+        self.all_sprites.update()
 
-        if event.type == pygame.QUIT:
-            sys.exit()
+    def draw_grid(self):
+        for x in range(0, WIDTH, TILESIZE):
+            pg.draw.line(self.screen, LIGHTGREY, (x, 0), (x, HEIGHT))
+        for y in range(0, HEIGHT, TILESIZE):
+            pg.draw.line(self.screen, LIGHTGREY, (0, y), (WIDTH, y))
+
+    def draw(self):
+        self.screen.fill(BGCOLOR)
+        self.draw_grid()
+        self.all_sprites.draw(self.screen)
+        pg.display.flip()
+
+    def events(self):
+        # catch all events here
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                self.quit()
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_ESCAPE:
+                    self.quit()
+                if event.key == pg.K_a:
+                    self.player.move(dx=-1)
+                if event.key == pg.K_d:
+                    self.player.move(dx=1)
+                if event.key == pg.K_w:
+                    self.player.move(dy=-1)
+                if event.key == pg.K_s:
+                    self.player.move(dy=1)
+
+    def show_start_screen(self):
+        pass
+
+    def show_go_screen(self):
+        pass
+
+# create the game object
+g = Game()
+g.show_start_screen()
+while True:
+    g.new()
+    g.run()
+    g.show_go_screen()
